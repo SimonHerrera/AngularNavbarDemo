11d49ee57f6d5e0d937aacb3f713f303f55097db
Simon Herrera <simonherrera.tech@gmail.com>
2017-01-10 22:29:31 -0600

    Update spacing and returns at end of files

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import { NavbarComponent} from './components/navbar/navbar.component';
-import { JumbotronComponent} from './components/jumbotron/jumbotron.component';
-import { HomeComponent} from './components/pages/home.component';
-import { AboutComponent} from './components/pages/about.component';
-import { ContactComponent} from './components/pages/contact.component';
-import { routing} from './app.routing';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { HomeComponent } from './components/pages/home.component';
+import { AboutComponent } from './components/pages/about.component';
+import { ContactComponent } from './components/pages/contact.component';
+import { routing } from './app.routing';
 
 
 @NgModule({
